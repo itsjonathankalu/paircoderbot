78e63dfa0c8487b15b45d819783e1e2db39b71bc
itsdevnate <jonathankalu313@gmail.com>
2025-09-01 18:08:50 +0100

    model update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const webhookPath = "/new-message";
 const genAI = new GoogleGenerativeAI(geminiKey);
 // const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 app.post(webhookPath, async (req, res) => {
   const { message } = req.body;
