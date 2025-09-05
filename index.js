2a2c8d1f2e3cd9a32dbbef85282fa0ff42035c7e
itsdevnate <jonathankalu313@gmail.com>
2025-09-05 19:21:05 +0100

    fix: revert memory feature and simplify bot logic

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const { Groq } = require("groq-sdk");
-const fs = require("fs");
-const { startPrompter } = require("./prompter");
 
 console.log("Starting server...");
 console.log("TELEGRAM_BOT_TOKEN exists:", !!process.env.TELEGRAM_BOT_TOKEN);
@@ -29,45 +27,6 @@ app.post(webhookPath, async (req, res) => {
   const chatId = message.chat.id;
   const text = message.text;
   const firstName = message.from?.first_name || "there"; // fetch Telegram first name
-
-  // Dynamic user registration
-  const usersFilePath = "./users.json";
-  let users = {};
-  try {
-    const usersData = fs.readFileSync(usersFilePath, "utf-8");
-    users = JSON.parse(usersData);
-  } catch (error) {
-    // If file doesn't exist, it will be created
-  }
-
-  const userExists = Object.values(users).some(batch => batch.some(user => user.chatId === chatId));
-
-  if (!userExists) {
-    const newUser = { chatId, name: firstName };
-    const batchKeys = Object.keys(users);
-    let targetBatch = "batch1";
-
-    if (batchKeys.length > 0) {
-        const lastBatch = batchKeys[batchKeys.length - 1];
-        if (users[lastBatch].length >= 2) {
-            targetBatch = `batch${batchKeys.length + 1}`;
-        } else {
-            targetBatch = lastBatch;
-        }
-    } 
-
-    if (!users[targetBatch]) {
-      users[targetBatch] = [];
-    }
-    users[targetBatch].push(newUser);
-
-    try {
-      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
-      console.log(`Added new user ${firstName} to ${targetBatch}`);
-    } catch (error) {
-      console.error("Error writing to users.json:", error);
-    }
-  }
 
   try {
     // Send "typing…" to Telegram
@@ -125,5 +84,4 @@ app.listen(PORT, () => {
   console.log(
     `curl -F "url=<YOUR_DEPLOYED_URL>${webhookPath}" ${telegramApi}/setWebhook`
   );
-  startPrompter();
 });
